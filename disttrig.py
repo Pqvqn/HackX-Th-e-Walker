34f24625b8df2cc888e74649bb959d7314784e15
Fantastik-open <fantastikbruh@gmail.com>
2022-10-09 01:18:07 -0400

    Trig Fixed

--- disttrig.py
+++ disttrig.py
@@ -1,12 +1,31 @@
+from cmath import pi
+from math import tan
 import cv2
-image = cv2.imread("Color_icon_red.png")
+
+image = cv2.imread("blob.jpg")
 dimensions = image.shape
 width = dimensions[1]
 height = dimensions[0]
+iAngle = 45
+iDist = 31.25
+iHeight = 31.25
+fAngle = 60
+fHeight = 32
+
 print("Width:  ", width)
 print("Height: ", height)
 
 hwidth = int(width/2)
-for i in range(int(height/300)):
-    color = image[i * 300, hwidth]
-    print (color)
+for i in range(height):
+    yVal = height - 1 - i
+    color = image[yVal, hwidth]
+    #print(color)
+    if color[0] == 0:
+        print("Obstruction Detected:")
+        print("Pixel x: ", hwidth)
+        print("Pixel y: ", yVal)
+        fDist = fHeight * tan((fAngle / 180) * pi)
+        print(fDist)
+        exit()
+
+print("No Obstruction Detected")
